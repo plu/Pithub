356389684bd94db647e46db10c09c7522fee143f
Johannes Plunien <plu@pqpq.de>
2011-06-25 19:06:35 +0400

    Improve Pithub::GitData::Trees docs.

--- lib/Pithub/GitData/Trees.pm
+++ lib/Pithub/GitData/Trees.pm
@@ -25,7 +25,54 @@ Create a Tree
 
 Examples:
 
-    $result = $p->git_data->trees->create( user => 'plu', repo => 'Pithub', data => { base_tree => 'df21b2660fb6' } );
+    $result = $p->git_data->trees->create(
+        user => 'plu',
+        repo => 'Pithub',
+        data => {
+            base_tree => 'df21b2660fb6'
+        }
+    );
+
+Parameters in C<< data >> hashref:
+
+=over
+
+=item *
+
+B<base_tree>: optional String of the SHA1 of the tree you want to
+update with new data
+
+=item *
+
+B<tree>: Array of Hash objects (of path, mode, type and sha)
+specifying a tree structure
+
+=item *
+
+B<tree.path>: String of the file referenced in the tree
+
+=item *
+
+B<tree.mode>: String of the file mode - one of 100644 for file
+(blob), 100755 for executable (blob), 040000 for subdirectory
+(tree), 160000 for submodule (commit) or 120000 for a blob that
+specifies the path of a symlink
+
+=item *
+
+B<tree.type>: String of blob, tree, commit
+
+=item *
+
+B<tree.sha>: String of SHA1 checksum ID of the object in the tree
+
+=item *
+
+B<tree.content>: String of content you want this file to have -
+GitHub will write this blob out and use that SHA for this entry.
+Use either this or tree.sha
+
+=back
 
 =cut
 
@@ -46,6 +93,14 @@ Get a Tree
 
     GET /repos/:user/:repo/git/trees/:sha
 
+Examples:
+
+    $result = $p->git_data->trees->get(
+        user => 'plu',
+        repo => 'Pithub',
+        sha  => 'df21b2660fb6'
+    );
+
 =item *
 
 Get a Tree Recursively
@@ -54,10 +109,12 @@ Get a Tree Recursively
 
 =back
 
-Examples:
-
-    $result = $p->git_data->trees->get( user => 'plu', repo => 'Pithub', sha => 'df21b2660fb6' );
-    $result = $p->git_data->trees->get( user => 'plu', repo => 'Pithub', sha => 'df21b2660fb6', recursive => 1 );
+    $result = $p->git_data->trees->get(
+        user      => 'plu',
+        repo      => 'Pithub',
+        sha       => 'df21b2660fb6',
+        recursive => 1,
+    );
 
 =cut
 
