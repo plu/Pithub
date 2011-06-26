32f7460fea4710c821551b7e70553264055c1336
Johannes Plunien <plu@pqpq.de>
2011-06-26 19:27:05 +0200

    Implement Pithub::Orgs::Teams->remove_member.

--- lib/Pithub/Orgs/Teams.pm
+++ lib/Pithub/Orgs/Teams.pm
@@ -277,11 +277,18 @@ it just remove them from the team.
 
 Examples:
 
-    my $result = $p->orgs->teams->remove_member( team_id => 1, user => 'plu' );
+    $result = $p->orgs->teams->remove_member(
+        team_id => 1,
+        user    => 'plu',
+    );
 
 =cut
 
 sub remove_member {
+    my ( $self, %args ) = @_;
+    croak 'Missing key in parameters: team_id' unless $args{team_id};
+    croak 'Missing key in parameters: user'    unless $args{user};
+    return $self->request( DELETE => sprintf( '/teams/%d/members/%s', $args{team_id}, $args{user} ) );
 }
 
 =head2 remove_repo
