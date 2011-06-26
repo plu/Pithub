6c4eb9aa6e1b186c31f2cd48fc0ffb8a46f8f478
Johannes Plunien <plu@pqpq.de>
2011-06-26 08:44:47 +0400

    Implement Pithub::Issues::Comments->get.

--- lib/Pithub/Issues/Comments.pm
+++ lib/Pithub/Issues/Comments.pm
@@ -67,11 +67,19 @@ Get a single comment
 
 Examples:
 
-    my $result = $p->issues->comments->get( repo => 'Pithub', user => 'plu', comment_id => 1 );
+    $result = $p->issues->comments->get(
+        repo       => 'Pithub',
+        user       => 'plu',
+        comment_id => 1,
+    );
 
 =cut
 
 sub get {
+    my ( $self, %args ) = @_;
+    croak 'Missing key in parameters: comment_id' unless $args{comment_id};
+    $self->_validate_user_repo_args( \%args );
+    return $self->request( GET => sprintf( '/repos/%s/%s/issues/comments/%d', $args{user}, $args{repo}, $args{comment_id} ) );
 }
 
 =head2 list
