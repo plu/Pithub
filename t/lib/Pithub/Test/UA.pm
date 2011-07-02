331faa2a2a6ac7d411071728c1a5f86d4903c304
Johannes Plunien <plu@pqpq.de>
2011-07-02 13:13:14 +0400

    Refactor test result loading.

--- t/lib/Pithub/Test/UA.pm
+++ t/lib/Pithub/Test/UA.pm
@@ -10,19 +10,15 @@ use namespace::autoclean;
 
 sub request {
     my ( $self, $request ) = @_;
-    my $path = sprintf '%s/http_response/%s', dirname(__FILE__), $request->uri->path;
+    my $path = sprintf '%s/http_response/%s/%s.%s', dirname(__FILE__), $request->uri->host, $request->uri->path, $request->method;
     my %query_form = $request->uri->query_form;
     while ( my ( $k, $v ) = each %query_form ) {
         $path .= sprintf '.%s-%s', $k, $v;
     }
-    my $res;
     if ( -f $path ) {
-        $res = read_file($path);
+        my $res = read_file($path);
+        return HTTP::Response->parse($res);
     }
-    elsif ( -d $path && -f "${path}/index" ) {
-        $res = read_file("${path}/index");
-    }
-    return HTTP::Response->parse($res) if $res;
     return HTTP::Response->new;
 }
 
